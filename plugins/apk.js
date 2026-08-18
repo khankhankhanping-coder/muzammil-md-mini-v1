3a47b4de2593ada8f338752464af99d5e7cc2d60
khankhankhanping-coder <khankhankhanping@gmail.com>
2026-08-18 14:28:06 +0500

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -30,7 +30,7 @@ cmd({
 *┃ 👑 VER :❯ ${app.file.vername}*
 *╰━━━━━━━━━━━━━━━┈⊷*
 
-*👑 BY :❯ ARSLAN-MD 👑*`;
+*👑 BY :❯ ᴍᴜᴢᴀᴍᴍɪʟ-ᴍᴅ 👑*`;
 
     await conn.sendMessage(from, { image: { url: app.icon }, caption }, { quoted: mek });
 
